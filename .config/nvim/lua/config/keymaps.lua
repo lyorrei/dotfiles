a237213219e309c9ed0fb3b88263713116c8e447
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2025-09-20 10:28:56 -0300

    feat: add tmux navigator and options

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Prevent `d` from copying to clipboard
+vim.keymap.set("n", "d", '"_d', { noremap = true, silent = true })
+vim.keymap.set("v", "d", '"_d', { noremap = true, silent = true })
+
+-- Paste without yanking replaced content in visual mode
+vim.keymap.set("x", "p", '"_dP', { noremap = true, silent = true })
+
+-- Center screen when scrolling up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C -u>zz")
